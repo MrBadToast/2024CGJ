743b0b4befada54a7c8843689dac3b5c21dd2882
BadToast <ship2042@gmail.com>
2024-07-15 13:10:44 +0900

    Mainscreen

--- Assets/Scripts/MainScreenUI.cs
+++ Assets/Scripts/MainScreenUI.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 public class MainScreenUI : MonoBehaviour
 {
     [SerializeField] private string nextSceneName;
+    public bool standbyForInput;
+
+    private void Update()
+    {
+        if (standbyForInput)
+        {
+            if (Input.anyKeyDown == true)
+            {
+                standbyForInput = false;
+                StartGame();
+            }
+        }
+    }
+
+    public void EnableInput()
+    {
+        standbyForInput = true;
+    }
 
     public void StartGame()
     {
